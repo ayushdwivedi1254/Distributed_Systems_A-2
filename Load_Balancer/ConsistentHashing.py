e52c0af274e4c047a4a800756889d3218367eef9
sarthak-nik <sarthaksham@gmail.com>
2024-01-20 01:06:00 +0530

    allocate pending

---
+++ Load_Balancer/ConsistentHashing.py
@@ -0,0 +1,86 @@
+from sortedcontainers import SortedSet
+
+
+class OrderedSet:
+    def __init__(self):
+        self.ordered_set = SortedSet()
+
+    def insert(self, value):
+        self.ordered_set.add(value)
+
+    def delete(self, value):
+        self.ordered_set.discard(value)
+
+    def upper_bound(self, value):
+        upper_bound_index = self.ordered_set.bisect_right(value)
+        if upper_bound_index < len(self.ordered_set):
+            return self.ordered_set[upper_bound_index]
+        else:
+            return self.ordered_set[0]
+
+
+class ConsistentHashing:
+    def __init__(self, N: int, M: int, K: int):
+        self.N = N  # Number of server containers
+        self.M = M  # Total number of slots in the consistent hash map
+        self.K = K  # Number of virtual servers for each server container
+
+        # Initialize data structures
+        self.serverIndices = OrderedSet()
+        self.serverNameToIndex: dict[str, list[int]] = {}
+        self.requestList = [[] for _ in range(M)]
+        self.serverList = ['' for _ in range(M)]
+
+    def calcServerHash(self, i: int, j: int):
+        hash = i*i+j*j+2*j+25
+        return hash % self.M
+
+    def add_server(self, server_number: int, server_name: str):
+        replicas = []
+
+        for j in range(self.K):
+            hash = self.calcServerHash(server_number, j)
+            index = hash
+            count = 1
+            # quadratic probing
+            while self.serverList[index] != '':
+                index = (hash+count*count) % self.M
+                count = count + 1
+
+            replicas.append(index)
+            self.serverList[index] = server_name
+            self.serverIndices.insert(index)
+
+        self.serverNameToIndex[server_name] = replicas
+
+    def calcRequestHash(self, id: int):
+        hash = id*id+2*id+17
+        return hash % self.M
+
+    def add_request(self, request_json: dict):
+        reqID = request_json["id"]
+        reqIndex = self.calcRequestHash(reqID)
+        self.requestList[reqIndex].append(reqID)
+        pass
+
+    def remove_server(self, server_number: int, server_name: str):
+        for ind in self.serverNameToIndex[server_name]:
+            self.serverList[ind] = ''
+            self.serverIndices.delete(ind)
+        self.serverNameToIndex.pop(server_name)
+
+    def allocate(self):
+        # TODO
+        pass
+
+    def debug_print(self):
+        print("All Server Indices:", self.serverIndices)
+        print("Server Name to Instances Indices:", self.serverNameToIndex)
+        print("Consistent Hashing serverList:", self.serverList)
+
+
+# # Example usage:
+# consistent_hashing = ConsistentHashing(N=3, M=512, K=9)
+# consistent_hashing.add_server(1, "Server1")
+# consistent_hashing.add_server(2, "Server2")
+# consistent_hashing.add_server(3, "Server3")
