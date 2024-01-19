91fd38ca02f94cc0499a19e103eb872b8960e95a
saptarshidec <saptarshi_dechaudhury@outlook.com>
2024-01-19 18:56:24 +0530

    Added spawning capability to load balancer

--- Load_Balancer/load_balancer.py
+++ Load_Balancer/load_balancer.py
@@ -1,10 +1,11 @@
 # load_balancer.py
 from flask import Flask, jsonify, request
-import requests
+import requests,os
 
 app = Flask(__name__)
 
 backend_server = "http://server:5001"
+count=0
 
 @app.route('/home', methods=['GET'])
 def proxy_request():
@@ -20,6 +21,23 @@ def proxy_request():
         'status_code': response.status_code,
         'data': response.text
     }), response.status_code
+
+@app.route('/spawn', methods=['GET'])
+def new_server():
+    global count
+    res=os.popen(f'sudo docker run --name container{count} --network distributed_systems_a-1_net1 --network-alias container{count} -d distributed_systems_a-1-server').read()
+    if len(res)==0:
+        return jsonify({
+        'status_code': 200,
+        'data': "Unable to start container"
+    }), 200
+    else:
+        count+=1
+        return jsonify({
+        'status_code': 200,
+        'data': "Successfully started container"
+    }), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
